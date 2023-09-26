c52a8cef77b5a2d98c64710ab58a4a02551f6e14
FacundoRinon <facuzionff@gmail.com>
2023-09-26 12:38:31 -0300

    First settings

---
+++ vite-project/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <>
+      <h1>Hola bb</h1>
+      <p>
+        Este es el momento en que esta todo dado para empezar a hacer cositas...
+      </p>
+    </>
+  );
+}
+
+export default Home;
