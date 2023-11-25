c1b5c173ccca1a903778950ba06ef713d155a2cb
FacundoRinon <facuzionff@gmail.com>
2023-11-25 15:38:06 -0300

    carrousel ready

--- vite-project/src/assets/constants.js
+++ vite-project/src/assets/constants.js
@@ -12,6 +12,12 @@ const categories = [
 
 const carrouselPics = [
   "https://phantom-expansion.unidadeditorial.es/92d3e5f909fc3902d36d8ab25c4e84de/crop/0x0/2044x1363/resize/414/f/jpg/assets/multimedia/imagenes/2021/12/07/16389099071788.jpg",
+  "https://cupfulofkale.com/wp-content/uploads/2020/05/Creamy-Vegan-Wild-Mushroom-Risotto-720x720.jpg",
+  "https://www.allrecipes.com/thmb/2AmYhc4iI0VjBTikhAuCmnbr4kg=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/220987-Baked-BBQ-Baby-Back-Ribs-mfs-042-2912e2b9d9b74bca8982c823452a6734.jpg",
+  "https://www.foodandwine.com/thmb/4Bt2yPC5El7vUBWZm_uFXD_vOyQ=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/California-Sushi-Rolls-FT-RECIPE0123-efc7744ed3e642c7864788a7c227facb.jpg",
+  "https://www.foodandwine.com/thmb/Wd4lBRZz3X_8qBr69UOu2m7I2iw=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/classic-cheese-pizza-FT-RECIPE0422-31a2c938fc2546c9a07b7011658cfd05.jpg",
+  "https://www.allrecipes.com/thmb/SoBuPU73KcbYHl3Kp3j8Xx4A3fc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/8805-CrispyFriedChicken-mfs-3x2-072-d55b8406d4ae45709fcdeb58a04143c2.jpg",
+  "https://www.eatingwell.com/thmb/ZgUTobMJAI_Q-zTpj273piX18h0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/EWL-250303-composed-salad-with-pickled-beets-smoked-tofu-Hero-02-9e2a9b7202a34d81908a4db78a927d57.jpg",
 ];
 
 export default { categories, carrouselPics };
--- vite-project/src/components/Carrousel/index.jsx
+++ vite-project/src/components/Carrousel/index.jsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-import carrouselPics from "../../assets/constants";
+import constants from "../../assets/constants";
 
 import "./index.scss";
 
 const Carrousel = () => {
-  console.log(carrouselPics);
+  const pics = constants.carrouselPics;
+  const [picIndex, setPicIndex] = useState(0);
+
+  const handleIndex = () => {
+    if (picIndex < pics.length - 1) {
+      setPicIndex(picIndex + 1);
+    } else {
+      setPicIndex(0);
+    }
+  };
+
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * pics.length);
+    setPicIndex(randomIndex);
+  }, []);
+
   return (
-    <div className="carrousel">
-      <h1>ngiweogniweo</h1>
+    <div onClick={() => handleIndex()} className="carrousel">
+      <img src={pics[picIndex]} alt="" />
     </div>
   );
 };
